c9478bcdb6f19abe8305c364f2362492677ff217
温满祥 <manxiang.wen@ucarinc.com>
2018-11-29 10:45:12 +0800

    [feat] add config server and config client

--- config-client/src/main/java/com/ucar/configclient/web/HelloController.java
+++ config-client/src/main/java/com/ucar/configclient/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.ucar.configclient;
+package com.ucar.configclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${hello}")
+    @Value("${server.port}")
     private String hello;
 
     @RequestMapping("/hello")
